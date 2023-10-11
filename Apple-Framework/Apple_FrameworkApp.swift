aff47df484f65469074d3d26beee5d07f27f86fc
Palatip Jantawong <palatipjant@outlook.com>
2023-10-11 08:01:38 +0700

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Palatip Jantawong on 11/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
