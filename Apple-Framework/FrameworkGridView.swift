8f01dcc1146175fd33d2c581ea6781fd7c1e41d1
Palatip Jantawong <palatipjant@outlook.com>
2023-10-11 09:41:07 +0700

    add image framework

---
+++ Apple-Framework/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Framework
+//
+//  Created by Palatip Jantawong on 11/10/2566 BE.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+} 
+
+#Preview {
+    FrameworkGridView()
+}
