90387dff590f5fa08d2634a7876b2c780f2b1aa7
Palatip Jantawong <palatipjant@outlook.com>
2023-10-15 13:42:49 +0700

    homepage

--- Apple-Framework/FrameworkGridView.swift
+++ Apple-Framework/FrameworkGridView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView{
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) {Framework in
+                        FrameworkTitleView(framework: Framework)
+                    }
+                }
+            }.navigationTitle("🍎 Frameworks")
+        }
     }
-} 
+}
 
 #Preview {
     FrameworkGridView()
+        .preferredColorScheme(.dark)
+}
+
+struct FrameworkTitleView: View{
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
 }
